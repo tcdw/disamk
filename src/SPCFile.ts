d9ea7e11359c21661033fde2f9e6159c6ec77cd2
tcdw <tcdw2011@gmail.com>
2019-11-20 00:59:07 +0800

    问题修正

--- src/SPCFile.ts
+++ src/SPCFile.ts
@@ -1,12 +1,9 @@
 class SPCFile {
-    static offset = 0x100;
-    raw: Buffer;
     mainHeader: Buffer;
     extraHeader: Buffer;
     aram: Buffer;
     dsp: Buffer;
     constructor(buffer: Buffer) {
-        this.raw = buffer;
         this.mainHeader = Buffer.alloc(0x100);
         this.extraHeader = Buffer.alloc(buffer.length - 0x10200);
         this.aram = Buffer.alloc(0x10000);
@@ -17,23 +14,23 @@ class SPCFile {
         buffer.copy(this.dsp, 0, 0x10100, 0x10180);
     }
     getBRR(id: number): Buffer {
-        const sampleIndexPtr = this.raw[0x1015D] * 0x100 + SPCFile.offset;
-        const samplePtr = this.raw.readUInt16LE(sampleIndexPtr + id * 4);
-        const sampleLoop = this.raw.readUInt16LE(sampleIndexPtr + id * 4 + 2) - samplePtr;
-        let sampleCurrentPtr = samplePtr + SPCFile.offset;
+        const sampleIndexPtr = this.dsp[0x5D] * 0x100;
+        const samplePtr = this.aram.readUInt16LE(sampleIndexPtr + id * 4);
+        const sampleLoop = this.aram.readUInt16LE(sampleIndexPtr + id * 4 + 2) - samplePtr;
+        let sampleCurrentPtr = samplePtr;
         while (true) {
             sampleCurrentPtr += 9;
-            if (this.raw[sampleCurrentPtr - 9] % 2 === 1) {
+            if (this.aram[sampleCurrentPtr - 9] % 2 === 1) {
                 break;
             }
-            if (sampleCurrentPtr > (0xFFFF + SPCFile.offset)) {
+            if (sampleCurrentPtr > (0xFFFF)) {
                 throw new Error('该 Sample 在 ARAM 结尾依然没有结束');
             }
         }
-        const sampleLength = sampleCurrentPtr - (samplePtr + SPCFile.offset);
+        const sampleLength = sampleCurrentPtr - samplePtr;
         const brr = Buffer.alloc(sampleLength + 2);
         brr.writeUInt16LE(sampleLoop, 0);
-        this.raw.copy(brr, 2, samplePtr + SPCFile.offset, sampleCurrentPtr);
+        this.aram.copy(brr, 2, samplePtr, sampleCurrentPtr);
         return brr;
     }
 }
