56f38a94de254f73afd4f64b3573a3b0b05bc695
tcdw <tcdw2011@gmail.com>
2019-11-20 10:30:40 +0800

    初始阶段

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,24 @@
+import { readFileSync } from "fs";
 import minimist from "minimist";
-import SPCFile from "./SPCFile";
+import { resolve } from "path";
+import parse from "./parse";
+
+const helpContent = `Usage: disamk [OPTIONS]
+
+Options:
+-h              Display this help information and exit
+-i file         (Required) Input SPC file
+-o dir          (Required) Output directory`;
 
 const args = minimist(process.argv.slice(2));
+
+if (args.h || args.help) {
+    process.stdout.write(helpContent);
+    process.exit(0);
+}
+if (!args.i || !args.o) {
+    process.stdout.write(helpContent);
+    process.exit(1);
+}
+const spc = readFileSync(resolve(process.cwd(), args.i));
+parse(spc);
---
+++ src/parse.ts
@@ -0,0 +1,9 @@
+function searchPattern(pattern: Array<number | number[]>) {
+    
+}
+
+function parse(spc: Buffer) {
+
+}
+
+export default parse;
