3ac097b608e4c28b067bb2cc007f946f3bc7cc03
tcdw <tcdw2011@gmail.com>
2021-02-01 18:54:58 +0800

    toggle logic

--- src/web.ts
+++ src/web.ts
@@ -1,9 +1,16 @@
 import JSZip from 'jszip';
+import mdui from 'mdui';
 import parse from './core/parse';
 import './scss/index.scss';
 
+const form = document.getElementById('main-form') as HTMLFormElement;
+const error = document.getElementById('error') as HTMLDivElement;
+const range = document.getElementById('range') as HTMLInputElement;
+const file = document.getElementById('file-receiver') as HTMLInputElement;
+const uploadBtn = document.getElementById('upload-btn') as HTMLLabelElement;
+
 function handleUpload(name: string, spc: ArrayBuffer) {
-    const { mmlFile, samples } = parse(spc, 10);
+    const { mmlFile, samples } = parse(spc, Number(range.value));
     const zip = new JSZip();
     const sampleDir = `${name}_samples`;
     zip.file(`${name}.txt`, `#amk 2\n#path "${sampleDir}"\n${mmlFile}`);
@@ -28,26 +35,35 @@ function handleUpload(name: string, spc: ArrayBuffer) {
     });
 }
 
-const file = document.getElementById('file-receiver') as HTMLInputElement;
-const uploadBtn = document.getElementById('upload-btn') as HTMLLabelElement;
-const error = document.getElementById('error') as HTMLDivElement;
-
 file.addEventListener('change', () => {
-    if (file && file.files !== null) {
+    if (file.files !== null) {
         const fileInfo = file.files[0];
-        const fileReader = new FileReader();
-        const content = new Blob();
-        fileReader.onload = (e) => {
-            if (e.target && e.target.result) {
-                error.style.display = 'none';
-                try {
-                    handleUpload(fileInfo.name, e.target.result as ArrayBuffer);
-                } catch (err) {
-                    error.style.display = 'block';
-                    error.textContent = err;
-                }
-            }
-        };
-        fileReader.readAsArrayBuffer(fileInfo);
+        const fileName = file.files[0].name;
+        uploadBtn.textContent = `Selected: ${fileName}`;
     }
 });
+
+form.addEventListener('submit', (f) => {
+    f.preventDefault();
+    if (!file.files?.length) {
+        error.style.display = 'block';
+        error.textContent = 'Please select a SPC file before starting';
+        return;
+    }
+    const fileInfo = file.files[0];
+    const fileReader = new FileReader();
+    fileReader.onload = (e) => {
+        if (e.target && e.target.result) {
+            error.style.display = 'none';
+            try {
+                handleUpload(fileInfo.name, e.target.result as ArrayBuffer);
+            } catch (err) {
+                error.style.display = 'block';
+                error.textContent = err;
+            }
+        }
+    };
+    fileReader.readAsArrayBuffer(fileInfo);
+});
+
+mdui.mutation();
